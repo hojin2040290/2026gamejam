ee88410bcd13caa1d83018c14e9dcacc552a85bd
김호진 <87836028+hojin2040290@users.noreply.github.com>
2026-02-22 04:38:32 +0900

    final commit

--- 2026-gmaejam/script/unlock3.gd
+++ 2026-gmaejam/script/unlock3.gd
@@ -2,5 +2,5 @@ extends TextEdit
 
 func _on_text_changed() -> void:
 	if (text == "HELP"):
-		get_tree().change_scene_to_file("res://scene/game4.tscn")
+		get_tree().change_scene_to_file("res://scene/communicationroom2.tscn")
 		Manager.Computer = true
