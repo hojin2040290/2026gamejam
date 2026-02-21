a363e104b84ba10cd2dce182f80e871eeb67141d
김호진 <87836028+hojin2040290@users.noreply.github.com>
2026-02-22 01:25:32 +0900

    finish3(기능 구현 완료)

--- 2026-gmaejam/script/game_4.gd
+++ 2026-gmaejam/script/game_4.gd
@@ -3,6 +3,7 @@ extends Node2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Manager.temp:
-		$Label.text = "Unlock the captain's cabin and communication room."
+		$Label.text = "Unlock the captain's cabin 
+		and communication room."
 	else:
 		$Label.text = ""
